80d1885d9b9f9fe13dba2cb57752f3b304f74a30
Dang Duong Hung <hanbdi@gmail.com>
2015-10-22 01:46:47 +0800

    - using slack-client api

--- src/bot/pokervnbot.js
+++ src/bot/pokervnbot.js
@@ -10,7 +10,6 @@ var PokerVnBot = function Constructor(token, autoReconnect, autoMark) {
     this.token = token;
     this.autoReconnect = autoReconnect;
     this.autoMark = autoMark;
-    //this.settings.name = this.settings.name || 'test-bot';
 
     this.user = null;
     this.db = null;
@@ -18,25 +17,48 @@ var PokerVnBot = function Constructor(token, autoReconnect, autoMark) {
 
 util.inherits(PokerVnBot, Bot);
 
-// PokerVnBot.on('message', function(message) {
-//     var channel = slack.getChannelGroupOrDMByID(message.channel);
-//     var user = slack.getUserByID(message.user);
-//
-//     if (message.type === 'message') {
-//         console.log(channel.name + ':' + user.name + ':' + message.text);
-//     }
-// });
-
 PokerVnBot.prototype.run = function() {
     PokerVnBot.super_.call(this, this.token, this.autoReconnect, this.autoMark);
     this.login();
-    this.on('open', this._onStart);
-    //this.on('message', this._onMessage);
-    //this.on('message', this._onMessage);
+    this.on('open', this._onOpen);
+    this.on('message', this._onMessage);
 }
 
-PokerVnBot.prototype._onMessage = function() {
-  console.log('onMessage');
+PokerVnBot.prototype._onOpen = function() {
+    var self = this;
+    var channels = Object.keys(self.channels)
+        .map(function (k) { return self.channels[k]; })
+        .filter(function (c) { return c.is_member; })
+        .map(function (c) { return c.name; });
+ 
+    var groups = Object.keys(self.groups)
+        .map(function (k) { return self.groups[k]; })
+        .filter(function (g) { return g.is_open && !g.is_archived; })
+        .map(function (g) { return g.name; });
+ 
+    //console.log('bot: ', self.self);
+    console.log('Welcome to Slack. You are ' + self.self.name + ' of ' + self.team.name);
+ 
+    if (channels.length > 0) {
+        console.log('You are in: ' + channels.join(', '));
+    }
+    else {
+        console.log('You are not in any channels.');
+    }
+ 
+    if (groups.length > 0) {
+       console.log('As well as: ' + groups.join(', '));
+    }
+    self._loadBotUser();
+}
+
+PokerVnBot.prototype._onMessage = function(message) {
+    var channel = this.getChannelGroupOrDMByID(message.channel);
+    var user = this.getUserByID(message.user);
+
+    if (message.type === 'message' && this._isDirectMessage(this.user.id, message.text)) {
+        console.log(channel.name + ':' + user.name + ':' + message.text);
+    }
 }
 
 PokerVnBot.prototype._onStart = function() {
@@ -47,11 +69,9 @@ PokerVnBot.prototype._onStart = function() {
 
 PokerVnBot.prototype._loadBotUser = function() {
     var self = this;
-    console.log('bot name', self.name);
-    console.log('this.users', self.users);
-    // this.user = this.users.filter(function (user) {
-    //     return user.name === self.name;
-    // })[0];
+    this.user = Object.keys(self.users)
+        .map(function (k) { return self.users[k]; })
+        .filter(function (u) { return u.name === self.self.name; })[0];
 }
 
 PokerVnBot.prototype._firstRunCheck = function() {
@@ -62,10 +82,20 @@ PokerVnBot.prototype._firstRunCheck = function() {
 }
 
 PokerVnBot.prototype._welcomeMessage = function() {
-    console.log('channel name: ', this.channels[3]);
+    console.log('channel name: ', this.channels);
     this.postMessageToChannel(this.channels[3].name, 'Hi guys, anyone there?', {as_user: true});
 }
 
+PokerVnBot.prototype._makeMention = function(userId) {
+    return '<@' + userId + '>';
+}
+
+PokerVnBot.prototype._isDirectMessage = function(userId, messageText) {
+    var userTag = this._makeMention(userId);
+    return messageText &&
+           messageText.length >= userTag.length &&
+           messageText.substr(0, userTag.length) === userTag;
+}
 
 
 module.exports = PokerVnBot;
